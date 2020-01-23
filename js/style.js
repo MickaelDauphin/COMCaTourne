4528162bf7e3503fd8333e5e5d572378a709436e
recrespe <remy.crespe@etu.uca.fr>
2020-01-23 16:32:03 +0100

    nav bar

--- js/style.js
+++ js/style.js
@@ -5,3 +5,19 @@ var swiper = new Swiper('.swiper-container', {
         disableOnInteraction: false,
     }
 });
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+
+        var height = $('.first-container').height();
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop >= height - 40) {
+            $('.nav-container').addClass('solid-nav');
+        } else {
+            $('.nav-container').removeClass('solid-nav');
+        }
+
+    });
+});
